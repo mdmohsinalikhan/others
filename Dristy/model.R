603afe70bf8be7e3d233a5579003f89a9a8d3219
mohsin <khanmdmohsinali@gmail.com>
2017-03-22 07:58:56 +0200

    commit from home

--- Dristy/model.R
+++ Dristy/model.R
@@ -1,14 +1,21 @@
 d = read.table(file="experiment.txt",header = TRUE)
-randomrows = sort(sample(nrow(d),size=15,replace=FALSE))
-training = d[randomrows,]
-test = d[-randomrows,]
-model1=lm(y1~x1+x2+x3+x4+x5,d,randomrows)
-model2=lm(y2~x1+x2+x3,d3,randomrows)
+#randomrows = sort(sample(nrow(d),size=15,replace=FALSE))
+#training = d[randomrows,]
+#test = d[-randomrows,]
+model1=lm(y1~x1+x2+x3+x4+x5,d)
+model2=lm(y2~x1+x2+x3,d)
 
-training_error_y1 = sqrt(mean((training$y1 - predict.lm(model1, training)) ^ 2))
-test_error_y1 = sqrt(mean((test$y1 - predict.lm(model1, test)) ^ 2))
+#error_y1 = sqrt(mean((d$y1 - predict.lm(model1, d)) ^ 2))
+#error_y2 = sqrt(mean((d$y2 - predict.lm(model2, d)) ^ 2))
 
-training_error_y2 = sqrt(mean((training$y2 - predict.lm(model2, training)) ^ 2))
-test_error_y2 = sqrt(mean((test$y2 - predict.lm(model2, test)) ^ 2))
+mean_error_y1 = mean((abs(d$y1 - predict.lm(model1, d))/d$y1)*100)
+sd_error_y1 = sd((abs(d$y1 - predict.lm(model1, d))/d$y1)*100)
+minimum_error_y1 = min((abs(d$y1 - predict.lm(model1, d))/d$y1)*100)
+maximum_error_y1 = max((abs(d$y1 - predict.lm(model1, d))/d$y1)*100)
 
 
+
+mean_error_y2 = mean((abs(d$y2 - predict.lm(model2, d))/d$y2)*100)
+sd_error_y2 = sd((abs(d$y2 - predict.lm(model2, d))/d$y2)*100)
+minimum_error_y2 = min((abs(d$y2 - predict.lm(model2, d))/d$y2)*100)
+maximum_error_y2 = max((abs(d$y2 - predict.lm(model2, d))/d$y2)*100)
